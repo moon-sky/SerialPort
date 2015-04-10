52d76bc2928a028730544638d5e074f8dc8c2465
moon-sky <511014405@qq.com>
2015-04-10 14:30:03 +0800

    初始化导入

---
+++ gen/org/winplus/serial/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.winplus.serial;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ obj/local/armeabi/objs/serial_port/SerialPort.o.d
@@ -0,0 +1,181 @@
+obj/local/armeabi/objs/serial_port/SerialPort.o: jni/SerialPort.c \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/termios.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/cdefs.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/cdefs_elf.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/android/api-level.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/ioctl.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/ioctl.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/ioctl.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm-generic/ioctl.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/ioctls.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/termbits.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/ioctl_compat.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/types.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/stdint.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/_types.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/machine/_types.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/_wchar_limits.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/posix_types.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/stddef.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/compiler.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/posix_types.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/types.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/types.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/machine/kernel.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/sysmacros.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/termios.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/termios.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/unistd.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/select.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/time.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/time.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/signal.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/limits.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/limits.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/limits.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/machine/internal_types.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/machine/limits.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/syslimits.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/page.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/string.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/malloc.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/signal.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm-generic/signal.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/sigcontext.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/siginfo.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm-generic/siginfo.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/sysconf.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/capability.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/pathconf.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/stat.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/stat.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/endian.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/endian.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/machine/endian.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/fcntl.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/fcntl.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/fcntl.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm-generic/fcntl.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/jni.h \
+ jni/SerialPort.h \
+ G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/android/log.h
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/termios.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/cdefs.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/cdefs_elf.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/android/api-level.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/ioctl.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/ioctl.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/ioctl.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm-generic/ioctl.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/ioctls.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/termbits.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/ioctl_compat.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/types.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/stdint.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/_types.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/machine/_types.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/_wchar_limits.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/posix_types.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/stddef.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/compiler.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/posix_types.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/types.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/types.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/machine/kernel.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/sysmacros.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/termios.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/termios.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/unistd.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/select.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/time.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/time.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/signal.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/limits.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/limits.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/limits.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/machine/internal_types.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/machine/limits.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/syslimits.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/page.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/string.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/malloc.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/signal.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm-generic/signal.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/sigcontext.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/siginfo.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm-generic/siginfo.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/sysconf.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/capability.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/pathconf.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/stat.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/stat.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/endian.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/sys/endian.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/machine/endian.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/fcntl.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/linux/fcntl.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm/fcntl.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/asm-generic/fcntl.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/jni.h:
+
+jni/SerialPort.h:
+
+G:/android-ndk-r9b/platforms/android-17/arch-arm/usr/include/android/log.h:
